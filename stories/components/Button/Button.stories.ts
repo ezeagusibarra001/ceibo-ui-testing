6182842b93bfbd9f0f36b985163c3dec44da86b7
ezeagusibarra001 <ibarra.ezequiel01@gmail.com>
2024-04-23 15:53:49 -0300

    Init Commit

---
+++ stories/components/Button/Button.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Button from '.'
+
+const meta = {
+    title: 'Ceibo/Button',
+    component: Button,
+    tags: ['autodocs'],
+    argTypes: {
+    },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    label: "Primary Button",
+    disabled: false,
+  },
+};
