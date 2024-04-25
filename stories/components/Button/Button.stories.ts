97137e85d88e057d6d077d82dc945239ad354a75
ezeagusibarra001 <ibarra.ezequiel01@gmail.com>
2024-04-25 15:01:19 -0300

    v2

--- stories/components/Button/Button.stories.ts
+++ stories/components/Button/Button.stories.ts
@@ -1,21 +1,17 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import Button from '.'
+import Button from ".";
 
 const meta = {
-    title: 'Ceibo/Button',
-    component: Button,
-    tags: ['autodocs'],
-    argTypes: {
-    },
+  title: "Ceibo/Button",
+  component: Button,
+  tags: ["autodocs"],
+  argTypes: {},
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-  args: {
-    label: "Primary Button",
-    disabled: false,
-  },
+  args: {},
 };
