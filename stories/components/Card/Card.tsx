fcd64bbfc100f8e5c8dfb83afa9653eb1d3cf6a0
ezeagusibarra001 <ibarra.ezequiel01@gmail.com>
2024-04-29 15:29:07 -0300

    tyry again

--- stories/components/Card/Card.tsx
+++ stories/components/Card/Card.tsx
@@ -2,10 +2,11 @@ import Image, { ImageProps } from "next/image";
 import React from "react";
 
 export interface Card {
-  image: ImageProps;
+  image?: ImageProps;
 }
 
 export default function Card({ image }: Card) {
+  if(!image) return <div>Cargando...</div>
   return (
     <article>
       <Image {...image} />
--- stories/components/index.ts
+++ stories/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button";
 export { default as Link } from "./LinkComponent";
 export { default as Card } from "./Card";
+export { default as FlatList } from "./FlatList";
