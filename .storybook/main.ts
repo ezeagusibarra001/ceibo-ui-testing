57babef8afc7c3237ecd6011313776c1a7cb7e2c
Tomas Apochian <tomasapochian@gmail.com>
2024-04-29 16:29:53 -0300

    add experimental React Server Components feature

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,6 +12,9 @@ const config: StorybookConfig = {
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
+  features: {
+    experimentalRSC : true
+  } ,
   framework: {
     name: "@storybook/nextjs",
     options: {},
