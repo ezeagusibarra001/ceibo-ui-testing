f7bde83ad33fe5fa7fe0c0d9ef2a47c30e01bdaa
ezeagusibarra001 <ibarra.ezequiel01@gmail.com>
2024-04-30 11:04:14 -0300

    v0.1.13

--- stories/components/Card/Card.stories.tsx
+++ stories/components/Card/Card.stories.tsx
@@ -14,11 +14,11 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    // image: {
-    //   src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0cQNVR8Dhjy6FgsxrizQ9PqPyTbGpmPYHLv2O7R9AFQ&s",
-    //   alt: "Messi",
-    //   width: 200,
-    //   height: 200
-    // }
+    image: {
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0cQNVR8Dhjy6FgsxrizQ9PqPyTbGpmPYHLv2O7R9AFQ&s",
+      alt: "Messi",
+      width: 200,
+      height: 200
+    }
   },
 };
