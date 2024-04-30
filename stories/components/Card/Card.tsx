7929aae9c3a5e7c613f586b38870ce8a6ef124ce
ezeagusibarra001 <ibarra.ezequiel01@gmail.com>
2024-04-30 11:02:21 -0300

    FINAL COUNTDOWN

--- stories/components/Card/Card.tsx
+++ stories/components/Card/Card.tsx
@@ -2,14 +2,13 @@ import Image, { ImageProps } from "next/image";
 import React from "react";
 
 export interface Card {
-  image?: ImageProps;
+  image: ImageProps;
 }
 
-export default function Card() {
-  // if(!image) return <div>Cargando...</div>
+export default function Card({ image }: Card) {
   return (
     <article>
-      <Image  src="https://images.pexels.com/photos/1716861/pexels-photo-1716861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt={'test image'}  width={100}height={100}  />
+      <Image {...image} />
     </article>
   );
 }
--- stories/components/LinkComponent/LinkComponent.tsx
+++ stories/components/LinkComponent/LinkComponent.tsx
@@ -2,12 +2,11 @@ import Link from "next/link";
 import React from "react";
 
 export interface LinkComponent {
-  text?: string;
-  href?: string;
+  text: string;
+  href: string;
 }
 
 export default function LinkComponent({ text, href }: LinkComponent) {
-  if(!text || !href) return <div>Cargando...</div>
   return (
     <Link target="_blank" href={href}>
       {text}
