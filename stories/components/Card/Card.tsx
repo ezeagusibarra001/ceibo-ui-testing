d360be3b2b854e793e4c47b9c5d1460130d4ec86
Tomas Apochian <tomasapochian@gmail.com>
2024-04-30 10:40:20 -0300

    test card story

--- stories/components/Card/Card.tsx
+++ stories/components/Card/Card.tsx
@@ -5,11 +5,11 @@ export interface Card {
   image?: ImageProps;
 }
 
-export default function Card({ image }: Card) {
-  if(!image) return <div>Cargando...</div>
+export default function Card() {
+  // if(!image) return <div>Cargando...</div>
   return (
     <article>
-      <Image {...image} />
+      <Image  src="https://images.pexels.com/photos/1716861/pexels-photo-1716861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt={'test image'}  width={100}height={100}  />
     </article>
   );
 }
